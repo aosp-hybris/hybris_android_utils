c02edb733b5b80d146bf7143ad7d06eff9bea6c2
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-05-20 15:12:08 +0800

    fix package-script path
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- package-script.sh
+++ package-script.sh
@@ -12,12 +12,17 @@ fi
 rm -rf $PACKAGE > /dev/null 2>&1
 
 # package android part
-find $OUT -print | \
-    tar acvf ${PACKAGE_TAR}  -T - --exclude=$OUT/obj --exclude=system.img --exclude=ramdisk.img --exclude=userdata.img
+TARGET_OBJ=${OUT/$ANDROID_BUILD_TOP\//}/obj
 
+find $OUT -print  | \
+    sed "s,$ANDROID_BUILD_TOP/,," | \
+    tar acvf ${PACKAGE_TAR} -T - --exclude=$TARGET_OBJ --exclude=system.img --exclude=ramdisk.img --exclude=userdata.img
+
+HOST_OBJ=${ANDROID_HOST_OUT/$ANDROID_BUILD_TOP\//}/obj
 # package linux host tools
-find $ANDROID_HOST_OUT -print | \
-    tar acvf ${PACKAGE_TAR}.1  -T - --exclude=$ANDROID_HOST_OUT/obj
+find $ANDROID_HOST_OUT -not \( -path $ANDROID_HOST_OUT/obj -prune \) -print  | \
+    sed "s,$ANDROID_BUILD_TOP/,," | \
+    tar acvf ${PACKAGE_TAR}.1  -T  - --exclude=$HOST_OBJ
 
 # concat two tar file
 tar --concatenate --file=${PACKAGE_TAR} ${PACKAGE_TAR}.1
@@ -27,3 +32,4 @@ rm -rf ${PACKAGE_TAR}.1 > /dev/null 2>&1
 
 # compress package
 gzip $PACKAGE_TAR
+#find $OUT  -not \( -path $ANDROID_PRODUCT_OUT/obj  \) -print |less
